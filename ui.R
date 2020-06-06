3d4809f0caac62d323b17804138dc387afd245d9
Bilgeyis <64548623+Bilgeyis@users.noreply.github.com>
2020-06-06 15:44:13 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,17 +1,7 @@
-#
-# This is the user-interface definition of a Shiny web application. You can
-# run the application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-# 
-#    http://shiny.rstudio.com/
-#
-
 
 library(shiny)
 source("dataprep.R")
 
-# Define UI for application that draws a histogram
 shinyUI(fluidPage(
   tabsetPanel(
     tabPanel("Macro View", 
